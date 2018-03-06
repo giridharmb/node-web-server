2e810da6cede0625ee17a46f901820e033bbb88c
Giridhar Bhujanga <gbhujanga@walmartlabs.com>
2018-03-06 15:08:05 +0530

    node testing framework.

---
+++ node-tests/utils/utils.test.js
@@ -0,0 +1,20 @@
+
+    const utils = require('./utils');
+
+    it('should add 2 numbers' , () => {
+        var res = utils.add(33,11);
+        if( res !== 44 ) {
+            throw new Error(`Expected 44, but got ${res}`);
+        }
+    });
+
+    it('should square a number' , () => {
+       var res = utils.square(5);
+       if(res !== 25) {
+           throw new Error(`Expected 25, but got ${res}`);
+       }
+
+       if(res != 25) {
+           throw new Error(`Expected 25, but got ${res}`)
+       }
+    });
--- server.js
+++ server.js
@@ -6,22 +6,24 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 
-hbs.registerPartials(__dirname + '/views/partials')
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine','hbs');
 
 app.use(express.static(__dirname + '/public'));
 
+
 app.use((req,res,next) => {
-  var now = new Date().toString();
-  var log = `${now} : ${req.method} ${req.url}`;
-  fs.appendFile('server.log', log + '\n', (err) => {
-    if(err) {
-      console.log('Unable to append to log file.');
-    }
-  });
-  next();
+    var now = new Date().toString();
+    var log = `${now} : ${req.method} ${req.url}`;
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to log file.');
+        }
+    });
+    next();
 });
+
 
 hbs.registerHelper('getCurrentYear' , () => {
   return new Date().getFullYear();
